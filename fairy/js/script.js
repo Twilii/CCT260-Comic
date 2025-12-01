7b08ecd579762fb4927307bdfa52cffd9b657391
Delilah Nhu Y Nguyen <nguy3978@dh2026pc10.utm.utoronto.ca>
2025-11-30 20:40:34 -0500

    Finished fairy, sword, and 12 heart path

---
+++ fairy/js/script.js
@@ -0,0 +1,12 @@
+// Hide text2 and Continue2 when page loads
+document.getElementById("text2").style.display = "none";
+document.getElementById("continue2").style.display = "none";
+
+// When Continue1 is clicked -> show text2, hide text1, swap buttons
+document.getElementById("continue1").onclick = function () {
+    document.getElementById("text1").style.display = "none";
+    document.getElementById("continue1").style.display = "none";
+
+    document.getElementById("text2").style.display = "block";
+    document.getElementById("continue2").style.display = "inline-block";
+};
